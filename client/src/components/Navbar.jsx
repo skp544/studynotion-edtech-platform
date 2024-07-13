5869ac4764fd4d189b26a143a89c0eee9e6696a2
kapilkaushik26 <kapilkaushik26@gmail.com>
2024-07-13 21:16:19 +0530

    Creating navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,9 +4,14 @@ import logo from "../assets/logos/Logo-Full-Light.png";
 import { NavbarLinks } from "../data";
 import { BsChevronDown } from "react-icons/bs";
 import { AiOutlineShoppingCart } from "react-icons/ai";
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
   const location = useLocation();
+
+  const { token } = useSelector((state) => state.auth);
+  const { user } = useSelector((state) => state.profile);
+  const { totalItems } = useSelector((state) => state.cart);
 
   const [subLinks, setSubLinks] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -92,9 +97,33 @@ const Navbar = () => {
         {/*todo:  need to do lot more work */}
         {/* Login / Signup / Dashboard */}
         <div className="hidden items-center gap-x-4 md:flex">
-          <Link to={"/dashboard/cart"} className="relative">
-            <AiOutlineShoppingCart className="text-2xl text-richblack-100" />
-          </Link>
+          {user && user?.accountType !== ACCOUNT_TYPE.INSTRUCTOR && (
+            <Link to={"/dashboard/cart"} className="relative">
+              <AiOutlineShoppingCart className="text-2xl text-richblack-100" />
+
+              {totalItems > 0 && (
+                <span className="absolute -bottom-2 -right-2 grid h-5 w-5 place-items-center overflow-hidden rounded-full bg-richblack-600 text-center text-xs font-bold text-yellow-100">
+                  {totalItems}
+                </span>
+              )}
+            </Link>
+          )}
+
+          {token === null && (
+            <Link to={"/login"}>
+              <button className="rounded-[8px] border border-richblack-700 bg-richblack-800 px-[12px] p-[8px] text-richblack-100 hover:scale-95 transition-all duration-200">
+                Log In
+              </button>
+            </Link>
+          )}
+          {token === null && (
+            <Link to={"/sign-up"}>
+              <button className="rounded-[8px] border border-richblack-700 bg-richblack-800 px-[12px] p-[8px] text-richblack-100 hover:scale-95 transition-all duration-200">
+                Sign Up
+              </button>
+            </Link>
+          )}
+          {token !== null && <ProfileDropdown />}
         </div>
       </div>
     </div>
