78bb42cfde198b046bda0d102b940e89fdf2b167
kapilkaushik26 <kapilkaushik26@gmail.com>
2024-07-13 16:25:08 +0530

    creating sections of homepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Navbar from "./components/Navbar";
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
