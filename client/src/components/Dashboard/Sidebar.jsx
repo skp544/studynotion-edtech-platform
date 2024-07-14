5a278511193f8b7908a78c3f389edc924d506d6a
kapilkaushik26 <kapilkaushik26@gmail.com>
2024-07-14 13:07:11 +0530

    creating sidebar

--- client/src/components/Dashboard/Sidebar.jsx
+++ client/src/components/Dashboard/Sidebar.jsx
@@ -1,7 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
+import { sidebarLinks } from "../../data";
+import SidebarLink from "./SidebarLink";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { VscSignOut } from "react-icons/vsc";
+import ConfirmationModal from "../ConfirmationModal";
+import { setToken } from "../../redux/slices/authSlice";
+import { setUser } from "../../redux/slices/profileSlice";
+import { resetCart } from "../../redux/slices/cartSlice";
+import toast from "react-hot-toast";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const { user, loading: profileLoading } = useSelector(
+    (state) => state.profile
+  );
+  const { loading: authLoading } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  // to keep track of confirmation modal
+  const [confirmationModal, setConfirmationModal] = useState(null);
+
+  if (profileLoading || authLoading) {
+    return (
+      <div className="grid h-[calc(100vh-3.5rem)] min-w-[220px] items-center border-r-[1px] border-r-richblack-700 bg-richblack-800">
+        <div className="spinner"></div>
+      </div>
+    );
+  }
+
+  const handleLogout = () => {
+    dispatch(setToken(null));
+    dispatch(setUser(null));
+
+    setConfirmationModal(null);
+    dispatch(resetCart());
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    toast.success("Logged Out");
+
+    navigate("/login");
+  };
+  return (
+    <>
+      <div className="flex h-[calc(100vh-3.5rem)] min-w-[220px] flex-col border-r border-r-richblack-700 bg-richblack-800 py-10">
+        <div className="flex flex-col">
+          {sidebarLinks.map((link) => {
+            if (link.type && user?.accountType !== link.type) return null;
+            return (
+              <SidebarLink key={link.id} link={link} iconName={link.icon} />
+            );
+          })}
+        </div>
+        <div className="mx-auto mt-6 mb-6 h-[1px] w-10/12 bg-richblack-700" />
+
+        <div className="flex flex-col">
+          <SidebarLink
+            link={{ name: "Settings", path: "/dashboard/settings" }}
+            iconName="VscSettingsGear"
+          />
+          <button
+            onClick={() =>
+              setConfirmationModal({
+                text1: "Are you sure?",
+                text2: "You will be logged out of your account.",
+                btn1Text: "Logout",
+                btn2Text: "Cancel",
+                btn1Handler: handleLogout,
+                btn2Handler: () => setConfirmationModal(null),
+              })
+            }
+            className="px-8 py-2 text-sm font-medium text-richblack-300"
+          >
+            <div className="flex items-center gap-x-2">
+              <VscSignOut className="text-lg" />
+              <span>Logout</span>
+            </div>
+          </button>
+        </div>
+      </div>
+      {confirmationModal && <ConfirmationModal modalData={confirmationModal} />}
+    </>
+  );
 };
 
 export default Sidebar;
